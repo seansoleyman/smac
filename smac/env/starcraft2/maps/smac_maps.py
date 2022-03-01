a24b7c92f9ecdad8b8ab88b2f22d569fa703ad4e
Sean Soleyman <seansoleyman@gmail.com>
2022-02-28 23:16:29 -0800

    Add 8m_switched map.

--- smac/env/starcraft2/maps/smac_maps.py
+++ smac/env/starcraft2/maps/smac_maps.py
@@ -32,6 +32,15 @@ map_param_registry = {
         "unit_type_bits": 0,
         "map_type": "marines",
     },
+    "8m_switched": {
+        "n_agents": 8,
+        "n_enemies": 8,
+        "limit": 120,
+        "a_race": "T",
+        "b_race": "T",
+        "unit_type_bits": 0,
+        "map_type": "marines",
+    },
     "25m": {
         "n_agents": 25,
         "n_enemies": 25,
